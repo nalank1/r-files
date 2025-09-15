62575355f2b493c120419bc497f5babb08716288
Nalan Kurnaz <nalan.kurnaz99@gmail.com>
2025-09-15 10:49:14 -0400

    Datasets+Starting Lab 2

---
+++ Introduction/power_analysis.r
@@ -0,0 +1,25 @@
+# power is probabiliyt of rejecting the null hypothesis hwen it is false
+# performed after acccepting a null hypothesis
+# a posteriori is common (after research)
+# factors affecting power
+# 1. decision criteria - related to alpha
+## the probability level at which one rejects the null hypothesis
+# 2. Sample size - 
+# larger the sample size, the refined the ability to see small effect defects
+# 3. effect size - larger effect size, larger the power
+
+## Types of power analyses
+# alpha - probability level at which one rejects the H0
+# beta - 1-power
+# a priori - useful when planning experiements
+###computes the sample soze required given beta, alpha and effect size
+
+# compromise - useful when the cost of type I and II errors can be quantified
+
+# criterion - 
+
+# post-hoc - 
+
+# practical
+## power analysis for t-test
+## for two independent means
