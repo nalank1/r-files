94c9374f747174467f7eb59d528b53b84553161a
Nalan Kurnaz <nalan.kurnaz99@gmail.com>
2025-09-25 20:57:12 -0400

    lab 2 done

---
+++ tidy-tuesday/fide-chess.r
@@ -0,0 +1,2 @@
+fide_ratings_august <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-09-23/fide_ratings_august.csv')
+fide_ratings_september <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-09-23/fide_ratings_september.csv')
