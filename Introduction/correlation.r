e9b872dc44e1d28846ef9dc3d9ea8d0c58d642bb
Nalan Kurnaz <nalan.kurnaz99@gmail.com>
2025-10-07 21:10:27 -0400

    lab3 - 6 done

---
+++ Introduction/correlation.r
@@ -0,0 +1,3 @@
+library(dplyr)
+
+sturgeon_data <- read.csv("https://raw.githubusercontent.com/nalank1/r-files/refs/heads/main/data-bioxx58/sturgeon.csv", header = TRUE, sep = ",", stringsAsFactors = TRUE)
